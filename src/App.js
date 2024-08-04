bdab857eebd4647ad8bef27f38e21cbe65d77887
Sabarish111111111 <sabarish.it22@bitsathy.ac.in>
2024-08-04 13:58:34 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Counter } from './features/counter/Counter';
+import  Counter  from './features/counter/Counter';
 import './App.css';
+import ProductList from './features/product-list/ProductList';
 
 function App() {
   return (
   <div className="App" >
     <Counter>
-
+       <ProductList></ProductList>
     </Counter>
   </div>
   );
