4b419429d609cf00fe0a9b0c7655ab860009aafa
Sabarish111111111 <sabarish.it22@bitsathy.ac.in>
2024-08-05 18:52:43 +0530

    v10

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
 
 
 import {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <Cart></Cart>,
   },
 ]);
 
