ab4d286f026a65fa1fb6ed1283c2378bcad1d6ab
Sabarish111111111 <sabarish.it22@bitsathy.ac.in>
2024-08-05 13:50:39 +0530

    v8

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -80,7 +80,7 @@ export default function Login() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{' '}
             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
+              Create an Account
             </a>
           </p>
         </div>
