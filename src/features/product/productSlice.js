3618eabd57a168fd3090482f0e966d431875d789
Sabarish111111111 <ravik60656@gmail.com>
2024-08-17 10:21:31 +0530

    17 18 2024 c6

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,24 +1,24 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './productAPI';
+import { fetchAllProducts } from './productAPI';
 
 const initialState = {
-  value: 0,
+  products : [],
   status: 'idle',
 };
 
 
 
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
+export const fetchAllProductsAsync = createAsyncThunk(
+  'product/fetchAllProducts',
+  async () => {
+    const response = await fetchAllProducts();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const productSlice = createSlice({
+  name: 'product',
   initialState,
 
   reducers: {
@@ -31,17 +31,17 @@ export const counterSlice = createSlice({
 
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(fetchAllProductsAsync.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.value += action.payload;
+        state.products = action.payload;
       });
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } = productSlice.actions;
 
 
 export const selectCount = (state) => state.counter.value;
@@ -49,4 +49,4 @@ export const selectCount = (state) => state.counter.value;
 
 
 
-export default counterSlice.reducer;
+export default productSlice.reducer;
