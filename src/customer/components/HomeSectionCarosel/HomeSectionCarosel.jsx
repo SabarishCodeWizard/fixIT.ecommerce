aa0700ba6a0212d8c1281523f88bf86599867e98
SabarishCodeWizard <ravik60656@gmail.com>
2024-08-22 18:13:24 +0530

    HomePageCrosel AliceCarousel and KeyboardArrowLeftIcon added

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import { Button } from '@headlessui/react';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const HomeSectionCarosel = () => {
 
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 4 },
+        1024: { items: 5.5 },
     };
 
 
-    const items = [1,1,1,1,1,1,1,1].map((item)=><HomeSectionCard/>)
+    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
 
     return (
-        <div>  <AliceCarousel
+        <div className='relative px-4 lg:px-8'>
+            <div className='relative p-5'>
 
-            items={items}
-            disableButtonsControls
-            autoPlay
-            autoPlayInterval={1000}
-            infinite
-            responsive={responsive}
+                <AliceCarousel
+                    items={items}
+                    disableButtonsControls
+                    infinite
+                    responsive={responsive}
 
-        /></div>
+                />
+
+                <Button
+                    variant="contained"
+                    className="z-50"
+                    sx={{ position: 'absolute', top: "8rem", right: "0rem", transform: "translateX(50%) rotate(90deg)" }}
+                    aria-label="next"
+                >
+                    <KeyboardArrowLeftIcon />
+                </Button>
+
+
+            </div>
+        </div>
     )
 }
 
