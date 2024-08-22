95939cfb9d39fb66c75ffcd5c7134031dde5c451
SabarishCodeWizard <ravik60656@gmail.com>
2024-08-22 17:23:02 +0530

    alice carousel home jsx added

--- src/customer/components/HomeCarosel/MainCrosel.jsx
+++ src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-
-const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
-};
-
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const MainCrosel = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        
-        controlsStrategy="alternate"
-    />
-);
+import { mainCarouselData } from './MainCaroselData';
 
 
-export default MainCrosel;
+
+const MainCrosel = () => {
+
+    const items = mainCarouselData.map((item) =>
+        <img className='cursor-pointer'
+            role='presentation'
+            src={item.image}
+            alt="" />
+    );
+
+
+
+    return (
+        <AliceCarousel
+
+            items={items}
+            disableButtonsControls
+            autoPlay
+            autoPlayInterval={1000}
+            infinite
+
+        />
+    )
+
+}
+
+
+    export default MainCrosel;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCrosel from '../../components/HomeCarosel/MainCrosel'
+
+const HomePage = () => {
+    return (
+        <div><MainCrosel></MainCrosel>
+         <div>Other section</div>
+        </div>
+    )
+}
+
+export default HomePage
