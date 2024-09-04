8f77577a996a3b34d5460e51dd36d055407f68b7
SabarishCodeWizard <ravik60656@gmail.com>
2024-09-04 15:20:46 +0530

    arrow button now working

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import { Button } from '@headlessui/react'; // Consider using a different Button component if necessary
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const HomeSectionCarousel = () => {
-
+    const [activeIndex, setActiveIndex] = useState(0)
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
         1024: { items: 5.5 },
     };
 
-    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+
+    const syncActiveIndex = ({ item }) => setActiveIndex(item)
+
+    const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
 
     return (
         <div className='relative px-4 lg:px-8 border'>
@@ -22,35 +27,40 @@ const HomeSectionCarousel = () => {
                     disableButtonsControls
                     infinite
                     responsive={responsive}
+                    disableDotsControls
+                    onSlideChange={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <Button
+                {activeIndex !== items.length - 5 && <Button
                     className="z-50 transition transform hover:scale-105 hover:shadow-lg"
-                    style={{ 
-                        position: 'absolute', 
-                        top: "8rem", 
-                        right: "0rem", 
-                        transform: "translateX(50%) rotate(90deg)", 
-                        backgroundColor: "white", 
-                        color: "black", 
+                    onClick={slideNext}
+                    style={{
+                        position: 'absolute',
+                        top: "8rem",
+                        right: "0rem",
+                        transform: "translateX(50%) rotate(90deg)",
+                        backgroundColor: "white",
+                        color: "black",
                         borderRadius: "10%",
                         padding: "10px", // Adjust padding for a better look
                     }}
                     aria-label="next"
                 >
                     <KeyboardArrowLeftIcon style={{ transform: "rotate(90deg)" }} />
-                </Button>
+                </Button>}
                 <Button
                     className="z-50 transition transform hover:scale-105 hover:shadow-lg"
-                    style={{ 
-                        position: 'absolute', 
-                        top: "8rem", 
-                        left: "0rem", 
-                        transform: "translateX(-50%) rotate(-90deg)", 
-                        backgroundColor: "white", 
-                        color: "black", 
+                    onClick={slidePrev}
+                    style={{
+                        position: 'absolute',
+                        top: "8rem",
+                        left: "0rem",
+                        transform: "translateX(-50%) rotate(-90deg)",
+                        backgroundColor: "white",
+                        color: "black",
                         borderRadius: "10%",
                         padding: "10px", // Adjust padding for a better look
-                        
+
                     }}
                     aria-label="next"
                 >
