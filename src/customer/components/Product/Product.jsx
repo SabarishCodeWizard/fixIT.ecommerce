3394a76c4ecaa32f9f0116894fdf179a825e32db
SabarishCodeWizard <ravik60656@gmail.com>
2024-09-11 09:52:54 +0530

    new Arrivals color size price discout range (mui radio js )  and availability  would be added

--- src/customer/components/Product/Product.jsx
+++ src/customer/components/Product/Product.jsx
@@ -18,6 +18,8 @@ import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from
 import { mens_kurta } from '../../../Data/men_kurta'
 import ProductCard from './ProductCard'
 import { filters, singleFilter } from './FilterData'
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+
 
 
 const sortOptions = [
@@ -99,7 +101,7 @@ export default function Product() {
                                         </DisclosurePanel>
                                     </Disclosure>
                                 ))}
-                                
+
                             </form>
                         </DialogPanel>
                     </div>
@@ -201,35 +203,47 @@ export default function Product() {
                                     </Disclosure>
                                 ))}
                                 {singleFilter.map((section) => (
+
                                     <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
-                                        <h3 className="-my-3 flow-root">
-                                            <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                                                <span className="font-medium text-gray-900">{section.name}</span>
-                                                <span className="ml-6 flex items-center">
-                                                    <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
-                                                    <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
-                                                </span>
-                                            </DisclosureButton>
-                                        </h3>
-                                        <DisclosurePanel className="pt-6">
-                                            <div className="space-y-4">
-                                                {section.options.map((option, optionIdx) => (
-                                                    <div key={option.value} className="flex items-center">
-                                                        <input
-                                                            defaultValue={option.value}
-                                                            defaultChecked={option.checked}
-                                                            id={`filter-${section.id}-${optionIdx}`}
-                                                            name={`${section.id}[]`}
-                                                            type="checkbox"
-                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                        />
-                                                        <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
-                                                            {option.label}
-                                                        </label>
+                                        {({ }) => (
+                                            <>  
+                                                <h3 className="-my-3 flow-root">
+                                                    <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                                       
+                                                        <FormLabel sx={{color:"black"}} className='text-gray-900' id="demo-radio-buttons-group-label">{section.name}</FormLabel>
+                                                        <span className="ml-6 flex items-center">
+                                                            <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
+                                                            <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
+                                                        </span>
+                                                    </DisclosureButton>
+                                                </h3>
+                                                <DisclosurePanel className="pt-6">
+                                                    <div className="space-y-4">
+                                                        <FormControl>
+                                                            <RadioGroup
+                                                                aria-labelledby="demo-radio-buttons-group-label"
+                                                                defaultValue="female"
+                                                                name="radio-buttons-group"
+                                                            >
+                                                                {section.options.map((option, optionIdx) => (
+
+
+
+                                                                    <>
+
+                                                                        <FormControlLabel value={option.id} control={<Radio />} label={option.label} />
+
+                                                                    </>
+
+                                                                ))}
+                                                            </RadioGroup>
+                                                        </FormControl>
+
                                                     </div>
-                                                ))}
-                                            </div>
-                                        </DisclosurePanel>
+                                                </DisclosurePanel>
+                                            </>
+
+                                        )}
                                     </Disclosure>
                                 ))}
                             </form>
